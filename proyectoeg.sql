0781276227d3f4f47aa7ad0d87f77aca53f963d4
Tomás Pinolini <tomaspinolini2003@gmail.com>
2024-05-08 18:05:35 -0300

    nuevo sql

--- proyectoeg.sql
+++ proyectoeg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 06, 2024 at 03:49 PM
+-- Generation Time: May 08, 2024 at 11:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,42 +24,163 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `locales`
+--
+
+CREATE TABLE `locales` (
+  `colLocal` int(11) NOT NULL,
+  `nombreLocal` varchar(100) NOT NULL,
+  `ubicacionLocal` varchar(50) NOT NULL,
+  `rubroLocal` varchar(20) NOT NULL,
+  `codUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `novedades`
+--
+
+CREATE TABLE `novedades` (
+  `codNovedad` int(11) NOT NULL,
+  `textoNovedad` varchar(200) NOT NULL,
+  `fechaDesdeNovedad` date NOT NULL,
+  `fechaHastaNovedad` date NOT NULL,
+  `tipoUsuario` enum('administrador','dueno de local','cliente') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promociones`
+--
+
+CREATE TABLE `promociones` (
+  `codPromo` int(11) NOT NULL,
+  `textoPromo` varchar(200) NOT NULL,
+  `fechaDesdePromo` date NOT NULL,
+  `fechaHastaPromo` date NOT NULL,
+  `categoria_cliente` enum('Inicial','Medium','Premium') NOT NULL,
+  `diasSemana` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`diasSemana`)),
+  `estadoPromo` enum('pendiente','aprobada','denegada') NOT NULL,
+  `codLocal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usos_promociones`
+--
+
+CREATE TABLE `usos_promociones` (
+  `codCliente` int(11) NOT NULL,
+  `codPromo` int(11) NOT NULL,
+  `fechaUsoPromo` date NOT NULL,
+  `estado` enum('enviada','aceptada','rechazada') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `tipo` varchar(50) NOT NULL
+  `codUsuario` int(11) NOT NULL,
+  `nombreUsuairo` varchar(100) NOT NULL,
+  `claveUsuario` varchar(8) NOT NULL,
+  `tipoUsuario` enum('administrador','dueno de local','cliente') NOT NULL,
+  `categoria_cliente` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `email`, `password`, `tipo`) VALUES
-(1, 'fewfewew@fsdf', 'fweew', 'cliente');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `locales`
+--
+ALTER TABLE `locales`
+  ADD PRIMARY KEY (`colLocal`),
+  ADD KEY `codUsuario` (`codUsuario`);
+
+--
+-- Indexes for table `novedades`
+--
+ALTER TABLE `novedades`
+  ADD PRIMARY KEY (`codNovedad`);
+
+--
+-- Indexes for table `promociones`
+--
+ALTER TABLE `promociones`
+  ADD PRIMARY KEY (`codPromo`),
+  ADD KEY `codLocal` (`codLocal`);
+
+--
+-- Indexes for table `usos_promociones`
+--
+ALTER TABLE `usos_promociones`
+  ADD KEY `codCliente` (`codCliente`),
+  ADD KEY `codPromo` (`codPromo`);
+
+--
 -- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`codUsuario`),
+  ADD UNIQUE KEY `nombreUsuairo` (`nombreUsuairo`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `locales`
+--
+ALTER TABLE `locales`
+  MODIFY `colLocal` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `novedades`
+--
+ALTER TABLE `novedades`
+  MODIFY `codNovedad` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `promociones`
+--
+ALTER TABLE `promociones`
+  MODIFY `codPromo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `codUsuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `locales`
+--
+ALTER TABLE `locales`
+  ADD CONSTRAINT `locales_ibfk_1` FOREIGN KEY (`codUsuario`) REFERENCES `usuarios` (`codUsuario`);
+
+--
+-- Constraints for table `promociones`
+--
+ALTER TABLE `promociones`
+  ADD CONSTRAINT `promociones_ibfk_1` FOREIGN KEY (`codLocal`) REFERENCES `locales` (`colLocal`);
+
+--
+-- Constraints for table `usos_promociones`
+--
+ALTER TABLE `usos_promociones`
+  ADD CONSTRAINT `usos_promociones_ibfk_1` FOREIGN KEY (`codCliente`) REFERENCES `usuarios` (`codUsuario`),
+  ADD CONSTRAINT `usos_promociones_ibfk_2` FOREIGN KEY (`codPromo`) REFERENCES `promociones` (`codPromo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
