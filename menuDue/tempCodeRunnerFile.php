20d755ef15f73a43fecf765b5ae7bd018dfa079e
Tomás Pinolini <tomaspinolini2003@gmail.com>
2024-05-27 16:25:19 -0300

    rutas php agregadas

--- menuDue/tempCodeRunnerFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-// foreach ($_POST["usos"] as $key => $value) {
-        //     $keys = explode(" ", $value);
-        //     $codPromo = $keys[0];
-        //     $codCliente = $keys[1];
-        //     $sql = "UPDATE usos_promociones SET estado = 'aprobada' WHERE codPromo = '$codPromo' AND codCliente = '$codCliente'";
-            // mysqli_query($mysqli, $sql);
-        // }
