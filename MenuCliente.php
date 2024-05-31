3f6d9d8e0a474a3242bce32aaad4a358b6d4a5c8
Tomás Pinolini <tomaspinolini2003@gmail.com>
2024-05-31 19:19:34 -0300

    mas estilo

--- MenuCliente.php
+++ MenuCliente.php
@@ -20,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu Cliente</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/menues.css">
+    <link rel="stylesheet" href="styles/menu.css">
 </head>
 <body>
     <div class="welcome">Bienvenido <?= htmlspecialchars($mailUsuario) ?>!</div>
@@ -30,9 +30,7 @@
         <div class="row"><a class="button-link"href="ver_novedades.php">Ver Novedades</a></div>
     </div>
     <div>
-        <button onclick="window.location.href='logout.php'">Log out</button>
+        <button class="button-link logout" onclick="window.location.href='logout.php'">Log out</button>
     </div>
-    
-    
 </body>
 </html>
